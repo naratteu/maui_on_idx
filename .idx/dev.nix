cb4e156caa92cbaaefae7e6aaafeedee2929a706
skfhddlg2 <skfhddlg2@gmail.com>
2025-07-13 11:05:51 +0000

    빌드실행자동화

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -14,8 +14,7 @@
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      "Dart-Code.flutter"
-      "Dart-Code.dart-code"
+      "muhammad-sammy.csharp"
     ];
     workspace = {
       # Runs when a workspace is first created with this `dev.nix` file
@@ -24,19 +23,21 @@
           dotnet workload config --update-mode workload-set
           dotnet workload update
           ";
-      };
+        };
       # To run something each time the workspace is (re)started, use the `onStart` hook
+      onStart = {
+        restore = "
+          dotnet workload restore
+          dotnet build -f net9.0-android -t:InstallAndroidDependencies -p:AndroidSdkDirectory=/home/user/.androidsdkroot -p:AcceptAndroidSdkLicenses=True
+          ";
+      };
     };
     # Enable previews and customize configuration
     previews = {
       enable = true;
       previews = {
-        web = {
-          command = ["flutter" "run" "--machine" "-d" "web-server" "--web-hostname" "0.0.0.0" "--web-port" "$PORT"];
-          manager = "flutter";
-        };
         android = {
-          command = ["flutter" "run" "--machine" "-d" "android" "-d" "localhost:5555"];
+          command = ["dotnet" "watch" "build" "-f" "net9.0-android" "-t:Run"];
           manager = "flutter";
         };
       };
--- README.md
+++ README.md
@@ -1,11 +1,5 @@
+# maui-on-idx
 
-`Firebase Studio`에 현 저장소를 Import합니다. (안드로이드 개발옵션 체크 필요)
+`Firebase Studio`에 현 저장소를 [Import](https://idx.google.com/import)합니다. (안드로이드 개발옵션 체크 필요)
 
-임포트 완료후 아래 명령어로 빌드 및 실행해봅니다.
-
-```bash
-dotnet workload restore
-dotnet build -f net9.0-android -t:InstallAndroidDependencies -p:AndroidSdkDirectory=/home/user/.androidsdkroot -p:AcceptAndroidSdkLicenses=True
-dotnet build -f net9.0-android
-dotnet build -f net9.0-android -t:Run
-```
+임포트 완료후 IDE상에서 앱 실행까지 자동으로 완료되어야 합니다.
